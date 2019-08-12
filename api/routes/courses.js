52c5588649d6a08a29d1b58dda1d125b82eb9b56
banobepascal <banobepascal@gmail.com>
2019-08-12 11:20:42 +0300

    adding get route

---
+++ api/routes/courses.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const courses = [
+    {id: 1, name: "course1"},
+    {id: 2, name: "course2"},
+    {id: 3, name: "course3"}
+];
+
+router.get('/', (req, res) => {
+    res.send(courses);
+});
