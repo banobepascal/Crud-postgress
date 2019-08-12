96819344dc9e5c9d70d373e0716263ddc4786339
banobepascal <banobepascal@gmail.com>
2019-08-12 11:50:00 +0300

    adding post requests

--- api/routes/courses.js
+++ api/routes/courses.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
+
+router.use(express.json());
 
 const courses = [
     {id: 1, name: "course1"},
@@ -11,6 +14,23 @@ router.get('/courses', (req, res) => {
     res.send(courses);
 });
 
+router.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    
+    courses.push(course);
+    res.send(course);
 
+});
+
+function validateInput(course) {
+    const schema = {
+        name: Joi.string().min(5).required()
+    };
+
+    return Joi.validate(course, schema);
+};
 
 module.exports = router;
