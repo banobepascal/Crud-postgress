4e9e18a3d6427373dff284468041c8d9628ed09e
banobepascal <banobepascal@gmail.com>
2019-08-12 12:42:42 +0300

    setting up connection to database

--- api/routes/courses.js
+++ api/routes/courses.js
@@ -25,16 +25,26 @@ router.post('/courses', (req, res) => {
 
 });
 
-router.put('/courses/:id', (req, res)=> {
+router.put('/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send("Course not found");
 
     const {error} = validateInput(req.body);
-    if (error) return res.status(400).send(result.error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
     course.name = req.body.name;
     res.send(course);
 
+}); 
+
+router.delete('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("Course not found");
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
 });
 
 router.get('/courses/:id', (req, res) => {
