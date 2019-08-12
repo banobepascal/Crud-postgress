800ee6fd9a0ff9e3a3571a18f9f140e17f309425
banobepascal <banobepascal@gmail.com>
2019-08-12 22:41:17 +0300

    finishing make db table

--- database/db.js
+++ database/db.js
@@ -13,3 +13,35 @@ const pool = new pg.Pool(config);
 pool.on('connect', () => {
   console.log('Connected to database');
 })
+
+const createTables = () => {
+  const profileTable = `CREATE TABLE IF NOT EXISTS
+      profiles(
+        id SERIAL PRIMARY KEY,
+        profile_name VARCHAR(128) NOT NULL,
+        profile_age SERIAL(3) NOT NULL,
+        profile_contact SERIAL(15) NOT NULL,
+        profile_status VARCHAR(128) NOT NULL
+      )`;
+      pool.query(profileTable)
+        .then((res) => {
+          console.log(res);
+          pool.end();
+        })
+        .catch((err) => {
+          console.log(err);
+          pool.end();
+        });
+};
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+module.exports = {
+  createTables,
+  pool,
+};
+
+require('make-runnable');
