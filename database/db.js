6d6daaf961a967782832eaa05e6f1083cb09d255
banobepascal <banobepascal@gmail.com>
2019-08-12 22:19:03 +0300

    adding db credentials

--- database/db.js
+++ database/db.js
@@ -1,6 +1,15 @@
 const pg = require('pg');
 
-module.exports = function() {
-    pg.connect('')
-  .then(() => winston.info('Connected to MongoDB...'))
-}
+const config = {
+  user: 'recipe',
+  database: 'recipe-book',
+  password: 'recipe',
+  port: 5432,
+  max: 20,
+};
+
+const pool = new pg.Pool(config);
+
+pool.on('connect', () => {
+  console.log('Connected to database');
+})
