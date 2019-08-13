176517e524c96d0b877f8dd8843a5c81b5c852bf
banobepascal <banobepascal@gmail.com>
2019-08-13 15:55:41 +0300

    made some changes

--- migrations/dbquery.js
+++ migrations/dbquery.js
@@ -1,15 +1,23 @@
-const users = `
-    CREATE TABLE IF NOT EXISTS users(
-        id SERIAL NUMBER PRIMARY KEY,
+const users = `CREATE TABLE IF NOT EXISTS 
+        users(
+        id SERIAL PRIMARY KEY,
         first_name  VARCHAR(20) NOT NULL,
         last_name VARCHAR(20) NOT NULL,
         email TEXT UNIQUE NOT NULL,
         ON DELETE CASCADE
-)`;
+);`;
+
+const authentication = `
+CREATE TABLE IF NOT EXISTS authentication(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(150) UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);`;
+
 
 const dropTables = `
         DROP TABLE IF EXISTS users cascade;
 `;
 
-module.exports = `${users}`;
+module.exports = `${users}${authentication}`;
 module.exports = dropTables;
