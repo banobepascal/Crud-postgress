6a6779401e5ef7464c379db00023b5d07b4dab8b
banobepascal <banobepascal@gmail.com>
2019-08-13 20:55:26 +0300

    changes made

--- migrations/dbSetup.js
+++ migrations/dbSetup.js
@@ -1,58 +1,64 @@
-// import { users,  authentication } from './dbquery';
-// import setupTables from './dbquery';
-const setupTables = require('./dbquery');
-const pg = require('pg');
+const pg = require("pg");
 
 const config = {
-  user: 'profile_app',
-  database: 'profile',
-  password: 'profile_app',
+  user: "profile_app",
+  database: "profile",
+  password: "profile_app",
   port: 5432,
-  max: 20,
+  max: 20
 };
 
 const pool = new pg.Pool(config);
 
-pool.on('connect', () => {
-  console.log('Connected to database');
+pool.on("connect", () => {
+  console.log("Connected to database");
 });
 
-
-
-
 const createTables = () => {
-  // const profileTable = `CREATE TABLE IF NOT EXISTS
-  //     profiles(
-  //       id SERIAL PRIMARY KEY,
-  //       profile_name VARCHAR(128) NOT NULL,
-  //       profile_age INT NOT NULL,
-  //       profile_contact VARCHAR(15) NOT NULL,
-  //       profile_status VARCHAR(128) NOT NULL
-  //     )`;
-  pool.query(setupTables, (error) => {
-    console.log('error', error);
-    pool.end();
-  });
- 
-      // pool.query(setupTables)
-      //   .then((res) => {
-      //     console.log(res);
-      //     pool.end();
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //     pool.end();
-      //   });
+  const users = `CREATE TABLE IF NOT EXISTS 
+        users(
+        id SERIAL PRIMARY KEY,
+        first_name  VARCHAR(20) NOT NULL,
+        last_name VARCHAR(20) NOT NULL,
+        email TEXT UNIQUE NOT NULL
+      )`;
+
+  const profileTable = `CREATE TABLE IF NOT EXISTS
+      profiles(
+        id SERIAL PRIMARY KEY,
+        profile_name VARCHAR(128) NOT NULL,
+        profile_age INT NOT NULL,
+        profile_contact VARCHAR(15) NOT NULL,
+        profile_status VARCHAR(128) NOT NULL
+      )`;
+
+  const authentication = `CREATE TABLE IF NOT EXISTS 
+      authentication(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(150) UNIQUE NOT NULL,
+      password TEXT NOT NULL
+      )`;
+
+  pool
+    .query(authentication)
+    .then(res => {
+      console.log(res);
+      pool.end();
+    })
+    .catch(err => {
+      console.log(err);
+      pool.end();
+    });
 };
 
-pool.on('remove', () => {
-  console.log('client removed');
+pool.on("remove", () => {
+  console.log("client removed");
   process.exit(0);
 });
 
 module.exports = {
   createTables,
-  pool,
+  pool
 };
 
-require('make-runnable');
+require("make-runnable");
