28fa749d6b3528d8980373160e7957f241cce846
banobepascal <banobepascal@gmail.com>
2019-08-20 16:16:53 +0300

    making changes to code paradigm

--- api/routes/person.js
+++ api/routes/person.js
@@ -10,38 +10,6 @@ router.get("/", (req, res) => {
   res.send("Welcome Pascal u are doing great");
 });
 
-router.get("/person", (req, res) => {
-  pool.connect((err, client, done) => {
-    const query = "SELECT * FROM users";
-    client.query(query, (error, result) => {
-      done();
-      if (error) {
-        res.status(400).json({ error });
-      }
-      if (result.rows < "1") {
-        res.status(404).send({
-          status: "Failed",
-          message: "No information for you"
-        });
-      } else {
-        res.status(200).send({
-          status: "Successful",
-          message: "You are served"
-        });
-      }
-    });
-  });
-});
 
-router.post("/people", (res, req) => {
-  const { first_name, last_name, email } = req.body
-
-  pool.query('INSERT INTO users (first_name, last_name, email) VALUES ($1, $2, $3)', [first_name, last_name, email], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(201).send(`User added with ID: ${results.insertId}`)
-  });
-});
 
 module.exports = router;
