8caf27905daa8a23c5190e9b0499a949d0482485
banobepascal <banobepascal@gmail.com>
2019-08-20 13:13:56 +0300

    adding get routes

--- api/routes/person.js
+++ api/routes/person.js
@@ -1,31 +1,35 @@
-const {pool} = require('../../migrations/dbSetup')
-const express = require('express');
+const { pool } = require("../../migrations/dbSetup");
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
-    res.send("Welcome Pascal u are doing great");
+router.get("/", (req, res) => {
+  res.send("Welcome Pascal u are doing great");
 });
 
-router.get('/person', (req, res) => {
-    pool.connect((err, client, done) => {
-        const query = 'SELECT * FROM users';
-        client.query(query, (error, result) => {
-            done();
-            if (error) {
-                res.status(400).json({error})
-            }
-            if(result.rows < '1'){
-                res.status(404).send({
-                    status : "Failed",
-                    message : "No information for you",
-                });
-            }
+router.get("/person", (req, res) => {
+  pool.connect((err, client, done) => {
+    const query = "SELECT * FROM users";
+    client.query(query, (error, result) => {
+      if (error) {
+        res.status(400).json({ error });
+      }
+      if (result.rows < "1") {
+        res.status(404).json({
+          status: "Failed",
+          message: "No information for you"
         });
+      } else {
+        res.status(200).json({
+          status: "Successful",
+          message: "You are served"
+        });
+      }
     });
+  });
 });
 
 module.exports = router;
