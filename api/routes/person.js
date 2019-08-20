d6433fb476e63d0cec9e484fc6247c4bda61f75a
banobepascal <banobepascal@gmail.com>
2019-08-20 14:22:35 +0300

    handling post request

--- api/routes/person.js
+++ api/routes/person.js
@@ -14,22 +14,50 @@ router.get("/person", (req, res) => {
   pool.connect((err, client, done) => {
     const query = "SELECT * FROM users";
     client.query(query, (error, result) => {
+        done();
       if (error) {
         res.status(400).json({ error });
       }
       if (result.rows < "1") {
-        res.status(404).json({
+        res.status(404).send({
           status: "Failed",
           message: "No information for you"
         });
       } else {
-        res.status(200).json({
+        res.status(200).send({
           status: "Successful",
           message: "You are served"
         });
       }
     });
   });
+});
+
+router.post("/people", (res, req) => {
+    const data = {
+        firstname : req.body.firstName,
+        lastname : req.body.lastName,
+        email : req.body.email,
+    }
+
+    pool.connect((err, client, done) => {
+        const query = 'INSERT INTO users(first_name, last_name, email) VALUES($1, $2,  $3) RETURNING *';
+        const values = [data.firstname, data.lastname, data.email];
+
+        client.query(query, values, (error, result) => {
+            done();
+            if(error) {
+                res.status(404).json({error});
+            } else {
+                res.status(200).send({
+                    status : "Successful",
+                    message : "You have added data",
+                    result : result.rows[0],
+                })
+            }
+
+        });
+    });
 });
 
 module.exports = router;
