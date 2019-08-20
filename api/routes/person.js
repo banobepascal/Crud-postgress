ac8436854c3a3558c0be7d5a0bf1a56d326f90e9
banobepascal <banobepascal@gmail.com>
2019-08-20 17:07:43 +0300

    created working post req

--- api/routes/person.js
+++ api/routes/person.js
@@ -1,15 +1,32 @@
 const { pool } = require("../../migrations/dbSetup");
 const express = require("express");
-const router = express.Router();
-const bodyParser = require("body-parser");
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+const getUsers = (req, res) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      res.status(400).json({error});
+    }
+    if (results.rows < '1'){
+      res.status(404).send({
+        status : "Failed",
+        message : "No information availble"
+      });
+    }
+    res.status(200).json(results.rows);
+  });
+};
 
-router.get("/", (req, res) => {
-  res.send("Welcome Pascal u are doing great");
-});
+const createUser = (req, res) => {
+  const {firstname, lastname, email} = req.body;
+    pool.query('INSERT INTO users(first_name, last_name, email) VALUES ($1, $2, $3)', [firstname, lastname, email], (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(202).send(`User has been created : ${results.insertId}`)
+    });
+};
 
-
-
-module.exports = router;
+module.exports = { 
+  getUsers,
+  createUser
+}
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const courses = require('./api/routes/courses');
-const person = require('./api/routes/person');
+const db = require('./api/routes/person');
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", courses);
-app.use("/person", person);
+app.get("/people", db.getUsers);
+app.post("/people", db.createUser);
 
 
 module.exports = app;
