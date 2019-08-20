a5a6a3bd56bda6d5bdcd73964f13c58f70af113e
banobepascal <banobepascal@gmail.com>
2019-08-20 17:45:29 +0300

    finished adding all routes

--- api/routes/person.js
+++ api/routes/person.js
@@ -1,16 +1,9 @@
 const { pool } = require("../../migrations/dbSetup");
-const express = require("express");
 
 const getUsers = (req, res) => {
   pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
     if (error) {
       res.status(400).json({ error });
-    }
-    if (results.rows < "1") {
-      res.status(404).send({
-        status: "Failed",
-        message: "No information availble"
-      });
     }
     res.status(200).send(results.rows);
   });
