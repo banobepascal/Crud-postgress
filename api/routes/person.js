fdc333291d4534321d4c0d1e774ea35deed83f93
banobepascal <banobepascal@gmail.com>
2019-08-20 17:42:57 +0300

    finished adding all routes

--- api/routes/person.js
+++ api/routes/person.js
@@ -4,12 +4,12 @@ const express = require("express");
 const getUsers = (req, res) => {
   pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
     if (error) {
-      res.status(400).json({error});
+      res.status(400).json({ error });
     }
-    if (results.rows < '1'){
+    if (results.rows < "1") {
       res.status(404).send({
-        status : "Failed",
-        message : "No information availble"
+        status: "Failed",
+        message: "No information availble"
       });
     }
     res.status(200).send(results.rows);
@@ -19,51 +19,58 @@ const getUsers = (req, res) => {
 const getUserByID = (req, res) => {
   const id = parseInt(req.params.id);
 
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
     if (error) {
-      res.status(400).json({error});
+      res.status(400).json({ error });
     }
-    res.status(200).send(results.rows);
+    res.status(202).send(results.rows);
   });
 };
 
 const createUser = (req, res) => {
-  const {firstname, lastname, email} = req.body;
-    pool.query('INSERT INTO users(first_name, last_name, email) VALUES ($1, $2, $3)', [firstname, lastname, email], (error, results) => {
+  const { firstname, lastname, email } = req.body;
+  pool.query(
+    "INSERT INTO users(first_name, last_name, email) VALUES ($1, $2, $3)",
+    [firstname, lastname, email],
+    (error, results) => {
       if (error) {
-        res.status(400).json({error});
+        res.status(400).json({ error });
       }
       res.status(200).send(results.rows[0]);
-    });
+    }
+  );
 };
 
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
-  const {firstname, lastname, email} = req.body;
-  pool.query('UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4', [firstname, lastname, email, id], (error, results) => {
-    if (error) {
-      res.status(400).json({error});
+  const { firstname, lastname, email } = req.body;
+  pool.query(
+    "UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4",
+    [firstname, lastname, email, id],
+    (error, results) => {
+      if (error) {
+        res.status(400).json({ error });
+      }
+      res.status(202).send(results.rows);
     }
-    res.status(200).send(results.rows);
-  });
+  );
 };
 
 const deleteUser = (req, res) => {
   const id = parseInt(req.params.id);
 
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
     if (error) {
-      res.status(400).json({error});
+      res.status(400).json({ error });
     }
-    res.status(200).send(results.rows);
+    res.status(202).send(results.rows);
   });
 };
 
-
-module.exports = { 
+module.exports = {
   getUsers,
   getUserByID,
   createUser,
   updateUser,
   deleteUser
-}
+};
