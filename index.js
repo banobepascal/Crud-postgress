25e9e99e11392ea9f73f7622a473c8a5c535af2a
banobepascal <banobepascal@gmail.com>
2019-08-20 12:17:42 +0300

    made changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const courses = require('./api/routes/courses');
+const person = require('./api/routes/person');
 const express = require('express');
 const app = express();
 
 app.use("/api", courses);
+app.use("/person", person);
 
 
 module.exports = app;
